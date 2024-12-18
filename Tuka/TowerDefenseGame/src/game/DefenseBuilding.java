f5790c3b0fe533a22268cef39e74e7c99e0e0660
Tuka3298 <takeshi062507250925@gmail.com>
2024-12-18 18:25:48 +0900

    test

--- Tuka/TowerDefenseGame/src/game/DefenseBuilding.java
+++ Tuka/TowerDefenseGame/src/game/DefenseBuilding.java
@@ -10,11 +10,18 @@ public class DefenseBuilding extends Building {
         super(x, y, 20, 300);
     }
 
+    // 攻撃可能か判定
+    public boolean CanAttack(double x, double y) {
+        if(x*x+y*y<2500) { return true; }
+        else             { return false; }
+    }
+
     @Override
     public void update(double deltaTime) {
         // クライアント側では特に処理なし
     }
 
+    // 施設自体の描画
     @Override
     public void draw(Graphics g) {
         // 防衛設備の描画（赤の四角）
@@ -31,5 +38,12 @@ public class DefenseBuilding extends Building {
         g.setColor(Color.GREEN);
         int hpBarWidth = (int)((health / 100.0) * 16);
         g.fillRect((int)x - 8, (int)y - 12, hpBarWidth, 3);
+    }
+
+    // 攻撃可能範囲の描画
+    public void drawRange(Graphics g) {
+        Color clr1 = new Color(255, 0, 0, 25);
+        g.setColor(clr1);
+        g.fillOval((int)x-25, (int)y-25, 50, 50);
     }
 }
